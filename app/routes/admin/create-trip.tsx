cd438df944b555d77725e59be299624c615c19fd
nafis09 <nafisahmed003@gmail.com>
2026-03-17 13:47:13 +0600

    feat: added submit option of trip

--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -2,10 +2,12 @@ import {Header} from "../../../components";
 import {ComboBoxComponent} from "@syncfusion/ej2-react-dropdowns";
 import type { Route } from './+types/create-trip'
 import {comboBoxItems, selectItems} from "~/constants";
-import {formatKey} from "~/lib/utils";
+import {cn, formatKey} from "~/lib/utils";
 import {LayerDirective, LayersDirective, MapsComponent} from "@syncfusion/ej2-react-maps";
-import {useState} from "react";
+import React, {useState} from "react";
 import {world_map} from "~/constants/world_map";
+import {ButtonComponent} from "@syncfusion/ej2-react-buttons";
+import {account} from "~/appwrite/client";
 
 
 export const loader = async () => {
@@ -50,7 +52,49 @@ const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
 
    })
 
-    const handleSubmit =  async () => {}
+    const [error, setError] = useState<String | null>(null);
+    const [loading, setLoading] = useState(false);
+
+
+    const handleSubmit =  async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setLoading(true);
+
+        if(
+            !formData.country ||
+            !formData.travelStyle ||
+            !formData.interest ||
+            !formData.budget ||
+            !formData.groupType
+        ) {
+            setError('Please fill out all fields');
+            setLoading(false)
+            return;
+        }
+
+        if(formData.duration < 1 || formData.duration > 10) {
+            setError('Duration must be between 1 and 10 days');
+            setLoading(false)
+            return;
+        }
+
+        const user = await account.get()
+        if(!user.$id) {
+            console.error("User not authenticated");
+            setLoading(false);
+            return;
+        }
+
+        try {
+            console.log('user', user)
+            console.log('formData', formData)
+        } catch (e){
+            console.error("Error generating trip", e);
+        } finally {
+            setLoading(false)
+        }
+    }
+
     const handleChange =(key: keyof TripFormData, value: string | number) => {
         setFormData({ ...formData, [key]: value })
     }
@@ -165,6 +209,27 @@ const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
                             </LayersDirective>
                         </MapsComponent>
                     </div>
+
+
+                    <div className="bg-gray-200 h-px w-full" />
+
+                    {error && (
+                        <div className="error">
+                            <p>{error}</p>
+                        </div>
+                    )}
+
+                    <footer className="px-6 w-full">
+                        <ButtonComponent type="submit"
+                        className="button-class !h-12 !w-full"
+                        disabled={loading}
+                        >
+                            <img src={`/assets/icons/${loading ? 'loader.svg' : 'magic-star.svg'}`} className={cn("size-5",{'animate-spin': loading})} />
+                            <span className="p-16-semibold text-white">
+                                {loading ? 'Generating...' : 'Generate Trip'}
+                            </span>
+                        </ButtonComponent>
+                    </footer>
                 </form>
             </section>
         </main>
